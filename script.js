fcf8072d36dab894e49b8641e607c819a503fab2
rtxa <usertxa@gmail.com>
2020-06-21 12:16:22 -0300

    Add partial Steam BBCode support

--- script.js
+++ script.js
@@ -40,7 +40,8 @@ mdToBB.list = function (body, ordered) {
   var closeTag = "[/list]";
 
   if (ordered) {
-    beginTag = "[list=1]"
+    beginTag = tagOListStart;
+    closeTag = tagOListEnd;
   }
 
   if ($("#addNewLine").checked) {
@@ -84,7 +85,7 @@ mdToBB.br = function () {
 };
 
 mdToBB.del = function (text) {
-  return '[s]' + Util.unescapeXML(text) + '[/s]';
+  return tagStrikeStart + Util.unescapeXML(text) + tagStrikeEnd;
 };
 
 mdToBB.link = function (href, title, text) {
@@ -115,6 +116,10 @@ mdToBB.image = function (href, title, text) {
 
 var markdownConverter = new MarkdownConverter(mdToBB);
 var changeYouTubeTag = false; // last MyBB version uses different tag for video
+var tagStrikeStart = "[s]";
+var tagStrikeEnd = "[/s]";
+var tagOListStart = "[list=1]";
+var tagOListEnd = "[/list]";
 
 function convert() {
   markdownConverter.convert(document.getElementById('markdownTextarea'), document.getElementById('bbcodeTextarea'));
@@ -132,8 +137,24 @@ function copyToClipboard() {
 
 }
 
+function defaultStyle() {
+  $("#header1").value = "";
+  $("#header2").value = "";
+  $("#header3").value = "";
+  $("#header4").value = "";
+  $("#formatCodeSpan").checked = false;
+  $("#addNewLineEndList").checked = false;
+  $("#addNewLine").checked = false;
+  changeYouTubeTag = false; 
+  tagStrikeStart = "[s]";
+  tagStrikeEnd = "[/s]";
+  tagOListStart = "[list=1]";
+  tagOListEnd = "[/list]";
+}
+
 // VBulletin
 function onClick_Style1() {
+  defaultStyle();
   $("#header1").value = "[color=DarkOrange][size=5][b]$1[/b][/size][/color]";
   $("#header2").value = "[color=RoyalBlue][size=4][b]$1[/b][/size][/color]";
   $("#header3").value = "[color=DodgerBlue][size=3][b]$1[/b][/size][/color]";
@@ -146,6 +167,7 @@ function onClick_Style1() {
 
 // phpBB
 function onClick_Style2() {
+  defaultStyle();
   $("#header1").value = "[color=DarkOrange][size=200][b]$1[/b][/size][/color]";
   $("#header2").value = "[color=RoyalBlue][size=160][b]$1[/b][/size][/color]";
   $("#header3").value = "[color=DodgerBlue][size=140][b]$1[/b][/size][/color]";
@@ -158,6 +180,7 @@ function onClick_Style2() {
 
 // MyBB
 function onClick_Style3() {
+  defaultStyle();
   $("#header1").value = "[color=DarkOrange][size=12][b]$1[/b][/size][/color]";
   $("#header2").value = "[color=RoyalBlue][size=6][b]$1[/b][/size][/color]";
   $("#header3").value = "[color=DodgerBlue][size=3][b]$1[/b][/size][/color]";
@@ -170,6 +193,7 @@ function onClick_Style3() {
 
 // phpBB ForoActivo
 function onClick_Style4() {
+  defaultStyle();
   $("#header1").value = "[h2][color=DarkOrange]$1[/color][/h2]";
   $("#header2").value = "[h2][color=RoyalBlue]$1[/color][/h2]";
   $("#header3").value = "[h3][color=DodgerBlue]$1[/color][/h3]";
@@ -177,5 +201,26 @@ function onClick_Style4() {
   $("#formatCodeSpan").checked = false;
   $("#addNewLineEndList").checked = false;
   $("#addNewLine").checked = false;
+  changeYouTubeTag = false;
+}
+
+// Steam BBCode
+// To do:
+// 1. Add [spoiler]string[/spoiler] (usar ~~string~~)
+// 2. Add [noparse]text[/noparse] (usar ``string``)
+// 3. Add [quote=yourname]text[/quote] (usar >[yourname] text)
+function onClick_Style5() {
+  defaultStyle();
+  $("#header1").value = "[h1]$1[/h1]";
+  $("#header2").value = "[h2]$1[/h2]";
+  $("#header3").value = "[h3]$1[/h3]";
+  $("#header4").value = "$1";
+  $("#formatCodeSpan").checked = true;
+  $("#addNewLineEndList").checked = false;
+  $("#addNewLine").checked = false;
+  tagStrikeStart = "[strike]";
+  tagStrikeEnd = "[/strike]";
+  tagOListStart = "[olist]";
+  tagOListEnd = "[/olist]";
   changeYouTubeTag = false;
 }
